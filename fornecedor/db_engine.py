d8934f2c25238642da9f6827fdaec6064b3c8dca
Luis Aquino <luissakino@gmail.com>
2022-02-15 17:51:24 -0300

    Commit teste.
    Commit teste.

--- fornecedor/db_engine.py
+++ fornecedor/db_engine.py
@@ -3,7 +3,7 @@ import configparser
 from sqlalchemy import create_engine
 from urllib.parse import quote
 
-
+##Teste
 def dbEngine(session):
     # Cria uma engine, objeto necessário para manipular objetos no banco de dados. A engine é a ponte entre a aplicação e o DB.
     # session refere-se a sessão do arquivo de configuração carregado no metodo .read()
